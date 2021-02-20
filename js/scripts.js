bc4aa524e35e00673b33b6d19c24dcfed49a3600
Felipe Montero Hofflinger <famh_94@hotmail.com>
2021-02-20 17:03:58 -0300

    Added NPM scripts for web deployment and configurations

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('#myCarousel').carousel({interval: 3000});
+    $('#carouselButton').click(function(){
+        if($('#carouselButton').children('span').hasClass('fa-pause')){
+            $('#myCarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+        }else{
+            $('#myCarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+        }
+    });
+    $('#loginLink').click(function (){
+        $('#loginModal').modal('show');
+    });
+    $('#reserveButton').click(function (){
+        $('#reserveModal').modal('show');
+    });
+});
